e9e05c8041cd8becc6c731f17fa06096e1cc52dc
IsaacM88 <45052253+IsaacM88@users.noreply.github.com>
2023-02-02 17:24:24 -0700

    Update trafficshaper.sh

--- trafficshaper.sh
+++ trafficshaper.sh
@@ -1,18 +1,30 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-# Define variables
-tcBin=`which tc` # Path to Traffic Control binary.
-ipBin=`which ip` # Path to ip binary.
-etBin=`which ethtool` # Path to ethtool binary.
-upLimit=8mbit # Upload bandwidth limit.
-downLimit=88mbit # Download bandwidth limit.
-if=enp6s0 # Your network interface name. Use "ip address" command to get interfaces.
-ifb=ifb0 # Incoming traffic will be redirected to this pseudo network interface.
+# User defined variables.
+upLimit="8mibit" # Upload bandwidth limit.
+downLimit="88mibit" # Download bandwidth limit.
+if="enp6s0" # Your network interface name. Run "ip link show" to get interfaces.
+
+# Default variables.
+ifb="ifb0" # Incoming traffic will be redirected to this pseudo network interface.
+suffixes=("" "bit" "Kibit" "kbit" "mibit" "mbit" "gibit" "gbit" "tibit" "tbit" "Bps" "KiBps" "KBps" "MiBps" "MBps" "GiBps" "GBps" "TiBps" "TBps")
 
 # Load Modules
 modprobe ifb
 modprobe sch_fq_codel
 modprobe act_mirred
+
+# Check for proper upload and download limit formatting.
+function checkLimit {
+	[[ "$1" =~ ^[[:digit:]]+.*$ && " ${suffixes[@]} " =~ " ${1//[[:digit:][:punct:]]/} " ]] || { echo "1"; return; }
+}
+
+# Check for valid variables.
+function check {
+	[[ -z $(checkLimit "$upLimit") ]] || { echo "upLimit is not formatted correctly: '$upLimit'"; return; }
+	[[ -z $(checkLimit "$downLimit") ]] || { echo "downLimit is not formatted correctly: '$downLimit'"; return; }
+	ip link show "$if" &> /dev/null || { echo "if is not a real interface: '$if'"; return; }
+}
 
 # Display arguments to the user.
 function halp {
@@ -25,36 +37,30 @@ function halp {
 # Display queuing disciplines attached to the network interfaces.
 function status {
 	echo -e "\n== Interface: $if ==\n"
-	$tcBin -s qdisc show dev $if
+	tc -s qdisc show dev "$if"
 	echo -e "\n== Interface: $ifb ==\n"
-	$tcBin -s qdisc show dev $ifb
+	tc -s qdisc show dev "$ifb"
 	echo ""
 }
 
 # Delete queuing disciplines and restore NIC offloading.
 function stop {
 	# Delete queuing disciplines, if they exist.
-	netRules=`$tcBin qdisc show dev $if`
-	if [[ "$netRules" == *"qdisc htb 1: root"* ]]; then
-		$tcBin qdisc delete dev $if root; fi
-	if [[ "$netRules" == *"qdisc ingress ffff: parent ffff:fff1"* ]]; then
-		$tcBin qdisc delete dev $if ingress; fi
+	netRules=$(tc qdisc show dev "$if")
+	[[ "$netRules" == *"qdisc htb 1: root"* ]] && tc qdisc delete dev "$if" root
+	[[ "$netRules" == *"qdisc ingress ffff: parent ffff:fff1"* ]] && tc qdisc delete dev "$if" ingress
 
-	ifbRules=`$tcBin qdisc show dev $ifb`
-	if [[ "$ifbRules" == *"qdisc htb 1: root"* ]]; then
-		$tcBin qdisc delete dev $ifb root; fi
+	ifbRules=$(tc qdisc show dev "$ifb")
+	[[ "$ifbRules" == *"qdisc htb 1: root"* ]] && tc qdisc delete dev "$ifb" root
 
 	# Offload network processing to NIC instead of CPU.
-	etConfig=`$etBin -k $if`
-	if [[ "$etConfig" == *"tcp-segmentation-offload"* ]]; then
-		$etBin -K $if tso on;	fi
-	if [[ "$etConfig" == *"generic-segmentation-offload"* ]]; then
-		$etBin -K $if gso on;	fi
-	if [[ "$etConfig" == *"generic-receive-offload"* ]]; then
-		$etBin -K $if gro on;	fi
+	etConfig=$(ethtool -k "$if")
+	[[ "$etConfig" == *"tcp-segmentation-offload"* ]] && ethtool -K "$if" tso on
+	[[ "$etConfig" == *"generic-segmentation-offload"* ]] && ethtool -K "$if" gso on
+	[[ "$etConfig" == *"generic-receive-offload"* ]] && ethtool -K "$if" gro on
 
 	# Turn off the ifb interface
-	$ipBin link set $ifb down
+	ip link set "$ifb" down
 }
 
 function start {
@@ -63,38 +69,40 @@ function start {
 
 	# Make sure the traffic shaper is not bypassed.
 	# These settings will offload network processing to the NIC when enabled.
-	etConfig=`$etBin -k $if`
-	if [[ "$etConfig" == *"tcp-segmentation-offload"* ]]; then
-		$etBin -K $if tso off;	fi
-	if [[ "$etConfig" == *"generic-segmentation-offload"* ]]; then
-		$etBin -K $if gso off;	fi
-	if [[ "$etConfig" == *"generic-receive-offload"* ]]; then
-		$etBin -K $if gro off;	fi
+	etConfig=$(ethtool -k "$if")
+	[[ "$etConfig" == *"tcp-segmentation-offload"* ]] && ethtool -K "$if" tso off
+	[[ "$etConfig" == *"generic-segmentation-offload"* ]] && ethtool -K "$if" gso off
+	[[ "$etConfig" == *"generic-receive-offload"* ]] && ethtool -K "$if" gro off
 
 	# Upload
 	
 	# Limit egress traffic using hbt and fq_codel.
-	$tcBin qdisc add dev $if root handle 1: htb default 1
-	$tcBin class add dev $if parent 1: classid 1:1 htb rate $upLimit
-	$tcBin qdisc add dev $if parent 1:1 fq_codel
+	tc qdisc add dev "$if" root handle 1: htb default 1
+	tc class add dev "$if" parent 1: classid 1:1 htb rate $upLimit
+	tc qdisc add dev "$if" parent 1:1 fq_codel
 
 	# Download
 
 	# Turn on the ifb interface.
-	$ipBin link set $ifb up
+	ip link set "$ifb" up
 
 	# Forward all incoming traffic to IFB where it will be rate limited.
-	$tcBin qdisc add dev $if handle ffff: ingress
-	$tcBin filter add dev $if parent ffff: protocol all u32 match u32 0 0 action mirred egress redirect dev $ifb
+	tc qdisc add dev "$if" handle ffff: ingress
+	tc filter add dev "$if" parent ffff: protocol all u32 match u32 0 0 action mirred egress redirect dev "$ifb"
 
 	# Limit egress traffic for ifb using hbt and fq_codel.
-	$tcBin qdisc add dev $ifb root handle 1: htb default 1
-	$tcBin class add dev $ifb parent 1: classid 1:1 htb rate $downLimit
-	$tcBin qdisc add dev $ifb parent 1:1 fq_codel
+	tc qdisc add dev "$ifb" root handle 1: htb default 1
+	tc class add dev "$ifb" parent 1: classid 1:1 htb rate $downLimit
+	tc qdisc add dev "$ifb" parent 1:1 fq_codel
 }
 
-if [ "$1" == "status" ]; then status
-elif [ "$1" == "stop" ]; then stop
-elif [ "$1" == "start" ]; then start
-else halp
+problem=$(check)
+if [[ -z "$problem" ]]; then
+	case "$1" in
+		"status") status ;;
+		"stop") stop ;;
+		"start") start ;;
+		*) halp ;;
+	esac
+else echo "$problem"
 fi
